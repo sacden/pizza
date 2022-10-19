abd2fc2a9db56cda067596ecbf59e7a3167bf4c0
sacden <sacden2009@gmail.com>
2022-10-20 01:35:24 +0200

    init: start project

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Vše', 'S masem', 'Vegetarianské', 'Grill', 'Palivé', 'Se sýrem'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((el, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}>
+            {el}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Categories;
