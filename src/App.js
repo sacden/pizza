e76ee11cb7f23b18684802eb35f24dbd4fd4c2c0
sacden <sacden2009@gmail.com>
2022-11-11 18:16:54 +0100

    refactor: use Outlet component as template & redirection to the main from unexisting pizza page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import './App.css';
 import './scss/app.scss';
-import Header from './components/Header';
+
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 import FullPizza from './pages/FullPizza';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/not-found" element={<NotFound />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<FullPizza />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const FullPizza = () => {
   const [pizza, setPizza] = React.useState();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     async function fetchPizza() {
@@ -13,6 +14,7 @@ const FullPizza = () => {
         setPizza(data);
       } catch (error) {
         alert('error');
+        navigate('/');
       }
     }
     fetchPizza();
